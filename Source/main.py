deaa62d5c75e0aa968c481a25bfe570176e8d790
alwaysnervous <bearmusic2017@gmail.com>
2024-04-30 15:28:49 +0300

    [TEST] pre-host

--- Source/main.py
+++ Source/main.py
@@ -190,7 +190,7 @@ def delete_job(job_id):
 
 @app.route("/")
 def portfolio():
-    city = request.headers.get('True-Client-Ip')
+    city = get_city_by_ip(request.headers.get('True-Client-Ip'))
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
     users = db_sess.query(User).all()
